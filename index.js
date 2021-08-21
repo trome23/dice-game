93c3f8594f5df9d19ef25402064b410e07c88e9e
Tyson Romero <romennoodles23@yahoo.com>
2021-08-20 23:08:06 -0600

    logging random # to console from 'roll dice' button

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 let player1Score = 0
 let player2Score = 0
-
 let player1Turn = false
+const player1Dice = document.querySelector("#player1Dice")
+const player2Dice = document.querySelector("#player2Dice")
+const player1ScoreBoard = document.querySelector("#player1Scoreboard")
+const player2ScoreBoard = document.querySelector("#player2Scoreboard")
+const rollBtn = document.querySelector("#rollBtn")
+const resetBtn = document.querySelector("#resetBtn")
+const message = document.querySelector("#message")
+
+rollBtn.addEventListener('click', function() {
+    const randNum = Math.floor(Math.random() * 6) + 1
+    console.log(randNum)
+})
